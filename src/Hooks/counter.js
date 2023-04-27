9f033a388446b687d98dacc158e0836648f532d6
abim.joshi <abim.joshi@midastechnologies.com.np>
2023-04-27 10:59:00 +0545

    add

--- src/Hooks/counter.js
+++ src/Hooks/counter.js
@@ -3,16 +3,15 @@ import Increment from './Increment';
 
 function Counter() {
     const [Count,setCount]=useState(0);
-    const newCount = () => {
-        setCount(Count + 1);
-      };
+    
 
     
   return (
     <div>
-        <Increment Count={Count}
-         newIncrement={newCount}
-        />
+        { Count <10 && (
+        <Increment Count={Count}    //resusing the properties//
+         newIncrement={()=>{setCount(Count+1)}}
+        />)}
         
     </div>
   );
